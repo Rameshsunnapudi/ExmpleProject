e59f2683ef904b969f571641b10eee645f32eae6
C-SGROUP\Rsunnapudi <rsunnapudi@c-sgroup.com>
2025-12-18 19:14:35 +0530

    first commit

---
+++ DemoProj/src/main/java/com/example/rest/TestController.java
@@ -0,0 +1,16 @@
+package com.example.rest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+	public String msg = "Welcome to My World...!!!";
+	
+	@GetMapping("/")
+	public String getMsg() {
+		return msg;
+	}
+
+}
---
+++ DemoProj/src/main/java/com/example/util/Book.java
@@ -0,0 +1,12 @@
+package com.example.util;
+
+import lombok.Data;
+
+@Data
+public class Book {
+
+	private String bookName;
+	private int bookPrice;
+	private String bookAuter;
+
+}
